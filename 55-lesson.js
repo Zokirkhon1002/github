826b136446eacdb9366056f879b792226fb848c8
Zokirkhon <zokirxonkotibxanov@gmail.com>
2021-07-19 10:03:57 +0900

    I learned from Farkhod Dadajanov, added first time from gitbash, index and 55-lesson.js

---
+++ 55-lesson.js
@@ -0,0 +1,10 @@
+async function getCustomerName() {
+    let name = await 'Zokirkhon';
+    console.log(name);
+    return name;
+}
+// getCustomerName().then(data => console.log(data));
+
+console.log(1);
+getCustomerName();
+console.log(2);
